df12bbf9ba5a0952c65bc3f909e6554a8b566868
Aron Elí Herreros <aroneh19@gmail.com>
2024-11-26 13:48:04 +0000

    fixed file names

--- src/views/BoardView.js
+++ src/views/BoardView.js
@@ -1,50 +1,125 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { useAppContext } from '../context/AppContext';
-import styles from './Styles/MainStyle';
-import List from '../components/Lists/ListCard';
-import {AddListButton} from '../components/AddButton/AddButton';
-import BackButton from '../components/BackButton/BackButton';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, Modal, TextInput, Alert } from "react-native";
+import Board from "../components/BoardView/BoardCard";
+import BackButton from "../components/BackButton/BackButton";
+import { AddBoardButton } from "../components/AddButton/AddButton";
+import { CustomModal } from "../components/Modal/Modal";
+
+import styles from "./Styles/MainStyle";
+import { useAppContext } from "../context/AppContext";
+
+const DUMMY_PHOTO =
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Dollarnote_siegel_hq.jpg/640px-Dollarnote_siegel_hq.jpg";
 
 const BoardView = ({ route, navigation }) => {
-    const { board } = route.params;
-    const { lists = [], tasks = [] } = useAppContext(); // Provide default empty arrays
+	const { boards, addBoard, deleteBoard, setBoards } = useAppContext();
+	const { userName } = route.params || {};
 
-    const boardLists = lists.filter((list) => list.boardId === board.id);
-    const listIds = boardLists.map((list) => list.id);
-    const boardTasks = tasks.filter((task) => listIds.includes(task.listId));
-    console.log(boardLists)
+	const [isModalVisible, setModalVisible] = useState(false);
+	const [editBoardId, setEditBoardId] = useState(null); // Track the board being edited
+	const [newBoardName, setNewBoardName] = useState("");
+	const [newBoardImage, setNewBoardImage] = useState("");
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.greeting}>
-                <Text>{board.name}</Text>
-            </Text>
-            <FlatList
-                data={boardLists}
-                renderItem={({ item }) => (
-                    <List
-                        name={item.name}
-                        color={item.color}
-                        onEdit={() => {
-                            setEditListId(item.id);
-                            setNewListName(item.name);
-                            setModalVisible(true);
-                        }}
-                        onDelete={() => handleDeleteList(item.id)}
-                        onPress={() => {
-                            navigation.navigate('ListView', { list: item });
-                        }}
-                    />
-                )}
-                keyExtractor={(item, index) => (item?.id ? item.id.toString() : index.toString())}
-                numColumns={1}
-                contentContainerStyle={styles.boardContainer}
-            />
-            <AddListButton onPress={() => setModalVisible(true)} />
-            <BackButton onPress={() => navigation.goBack()} />
-        </View>
-    );
+	const handleAddBoard = () => {
+		if (!newBoardName.trim()) {
+			// Check if the name is empty or only whitespace
+			Alert.alert("Error", "Board name is required!");
+			return;
+		}
+		if (!newBoardImage) {
+			// Check if the name is empty or only whitespace
+			Alert.alert("Error", "A Photo is required!");
+			return;
+		}
+		const newBoardId = boards.length + 1;
+		const newBoard = {
+			id: newBoardId,
+			name: newBoardName || `New Board ${newBoardId}`,
+			thumbnailPhoto: newBoardImage || DUMMY_PHOTO,
+		};
+
+		addBoard(newBoard);
+		resetModal();
+	};
+
+	const handleEditBoard = () => {
+		resetModal();
+		const updatedBoards = boards.map((board) =>
+			board.id === editBoardId
+				? {
+						...board,
+						name: newBoardName,
+						thumbnailPhoto: newBoardImage || board.thumbnailPhoto,
+				  }
+				: board
+		);
+		setBoards(updatedBoards);
+		resetModal();
+	};
+
+	const handleDeleteBoard = (boardId) => {
+		deleteBoard(boardId);
+	};
+
+	const resetModal = () => {
+		setModalVisible(false);
+		setEditBoardId(null);
+		setNewBoardName("");
+		setNewBoardImage("");
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.greeting}>
+				Hi, <Text style={styles.userName}>{userName}</Text>!
+			</Text>
+
+			<FlatList
+				data={boards}
+				renderItem={({ item }) => (
+					<Board
+						name={item.name}
+						thumbnailPhoto={item.thumbnailPhoto}
+						onEdit={() => {
+							setEditBoardId(item.id);
+							setNewBoardName(item.name);
+							setNewBoardImage(item.thumbnailPhoto);
+							setModalVisible(true);
+						}}
+						onDelete={() => handleDeleteBoard(item.id)}
+						onPress={() => {
+							navigation.navigate("ListView", { board: item });
+						}}
+					/>
+				)}
+				keyExtractor={(item, index) =>
+					item?.id ? item.id.toString() : index.toString()
+				}
+				numColumns={2}
+				contentContainerStyle={styles.boardContainer}
+			/>
+
+			<AddBoardButton onPress={() => setModalVisible(true)} />
+
+			<CustomModal
+				visible={isModalVisible}
+				onClose={resetModal}
+				onSave={editBoardId ? handleEditBoard : handleAddBoard}
+				title={editBoardId ? "Edit Board" : "Add New Board"}
+				inputs={[
+					{
+						placeholder: "Board Name",
+						value: newBoardName,
+						setValue: setNewBoardName,
+					},
+				]}
+				setInputs={[setNewBoardName]}
+				onImageSelected={setNewBoardImage}
+			/>
+
+			<BackButton onPress={() => navigation.goBack()} />
+		</View>
+	);
 };
 
 export default BoardView;
--- src/views/ListView.js
+++ src/views/ListView.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { useAppContext } from '../context/AppContext';
-import styles from './Styles/MainStyle';
-import {AddTaskButton} from '../components/AddButton/AddButton';
-import BackButton from '../components/BackButton/BackButton';
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import { useAppContext } from "../context/AppContext";
+import styles from "./Styles/MainStyle";
+import List from "../components/Lists/ListCard";
+import { AddListButton } from "../components/AddButton/AddButton";
+import BackButton from "../components/BackButton/BackButton";
 
 const ListView = ({ route, navigation }) => {
-    const { list } = route.params;
+	const { board } = route.params;
+	const { lists = [], tasks = [] } = useAppContext(); // Provide default empty arrays
 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={list}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => <Text>{item}</Text>}
-            />
-        
-        <AddTaskButton onPress={() => setModalVisible(true)} />
-        <BackButton onPress={() => navigation.goBack()} />
-        </View>
-    );
-}
+	const boardLists = lists.filter((list) => list.boardId === board.id);
+	const listIds = boardLists.map((list) => list.id);
+	const boardTasks = tasks.filter((task) => listIds.includes(task.listId));
+	console.log(boardLists);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.greeting}>
+				<Text>{board.name}</Text>
+			</Text>
+			<FlatList
+				data={boardLists}
+				renderItem={({ item }) => (
+					<List
+						name={item.name}
+						color={item.color}
+						onEdit={() => {
+							setEditListId(item.id);
+							setNewListName(item.name);
+							setModalVisible(true);
+						}}
+						onDelete={() => handleDeleteList(item.id)}
+						onPress={() => {
+							navigation.navigate("TaskView", { list: item });
+						}}
+					/>
+				)}
+				keyExtractor={(item, index) =>
+					item?.id ? item.id.toString() : index.toString()
+				}
+				numColumns={1}
+				contentContainerStyle={styles.boardContainer}
+			/>
+			<AddListButton onPress={() => setModalVisible(true)} />
+			<BackButton onPress={() => navigation.goBack()} />
+		</View>
+	);
+};
 
 export default ListView;
