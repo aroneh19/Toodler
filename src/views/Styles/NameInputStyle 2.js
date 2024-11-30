0bb9f7299b94b344cdf524f91c22cc0a2932d239
tommibent8 <144326353+tommibent8@users.noreply.github.com>
2024-11-30 15:05:22 +0000

    IDK

---
+++ src/views/Styles/NameInputStyle 2.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1e1e1e',
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#555',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
+
+export default styles;
